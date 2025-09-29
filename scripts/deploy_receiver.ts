1dd054b1e70f658993d297343df1aeb645d6c3ee
anassgono3-ctrl <anassgono3@gmail.com>
2025-09-29 21:23:21 +0100

    Refactor deployment process for Receiver and Vault

--- scripts/deploy_receiver.ts
+++ scripts/deploy_receiver.ts
@@ -6,20 +6,18 @@ async function main() {
 
   const Receiver = await ethers.getContractFactory("BalancerFlashJitReceiver");
   const receiver = await Receiver.deploy();
-  await receiver.deployed();
-  console.log("Receiver deployed:", receiver.address);
+  await receiver.waitForDeployment();
+  const receiverAddr = await receiver.getAddress();
+  console.log("Receiver deployed:", receiverAddr);
 
   const MockVault = await ethers.getContractFactory("MockVault");
   const vault = await MockVault.deploy();
-  await vault.deployed();
-  console.log("MockVault deployed:", vault.address);
-
-  // fund tokens & run a local test manually if desired.
+  await vault.waitForDeployment();
+  const vaultAddr = await vault.getAddress();
+  console.log("MockVault deployed:", vaultAddr);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
